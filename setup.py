9355ca838b710769359e0ab0036a0dce5648d79a
Keith Phillips <keithbphillips@gmail.com>
2025-10-08 22:02:25 -0500

    Bump version to 0.2.1-1 with critical bug fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="pinballux",
-    version="0.1.0",
+    version="0.2.1",
     author="Keith",
     description="A Visual Pinball frontend for Linux with multi-monitor support",
     long_description=long_description,
