2356e717b066bfe846b036a449dfa2963e5c02a4
Keith Phillips <keithbphillips@gmail.com>
2025-11-04 05:27:36 -0600

    Bump version to 0.5.0

--- pinballux/__init__.py
+++ pinballux/__init__.py
@@ -2,6 +2,6 @@
 PinballUX - A Visual Pinball frontend for Linux
 """
 
-__version__ = "0.3.0"
+__version__ = "0.5.0"
 __author__ = "Keith"
 __description__ = "A Visual Pinball frontend for Linux with multi-monitor support"
